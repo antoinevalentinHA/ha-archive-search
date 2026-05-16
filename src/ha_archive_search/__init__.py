b5956e2a15edc0d26dbec96d94399b02db584278
antoinevalentin <antoine.valentin@gmail.com>
2026-05-16 11:04:03 +0200

    Add minimal public surface to package __init__

---
+++ src/ha_archive_search/__init__.py
@@ -0,0 +1,14 @@
+from ha_archive_search.engine import (
+    Match,
+    SearchResult,
+    main,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Match",
+    "SearchResult",
+    "main",
+    "__version__",
+]
